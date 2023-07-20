27d8e7aa3642501f33719b7c459f29806a2f99bc
myselfshravan <myselfshravan@gmail.com>
2023-07-20 12:41:38 +0530

    delete line file updated

--- delete_lines.sh
+++ delete_lines.sh
@@ -1,18 +1,30 @@
 #!/bin/bash
 
-if [ "$#" -lt 2 ]; then
-    echo "Usage: $0 <word_to_delete> <file1> [file2] [file3] ..."
+# Check if the required arguments are provided
+if [ $# -lt 2 ]; then
+    echo "Usage: $0 <word> <file1> [<file2> ...]"
     exit 1
 fi
 
-word_to_delete="$1"
+word=$1
 shift
 
+# Iterate through each file
 for file in "$@"; do
-    if [ -f "$file" ]; then
-        sed -i "/\b$word_to_delete\b/d" "$file"
-        echo "Deleted lines containing '$word_to_delete' from $file."
-    else
-        echo "Error: $file is not a valid file."
+    # Check if the file exists and is readable
+    if [ ! -f "$file" ] || [ ! -r "$file" ]; then
+        echo "Error: $file is not a readable file."
+        continue
     fi
+    
+    # Create a temporary file to store the filtered content
+    temp_file=$(mktemp)
+    
+    # Delete lines containing the specified word and store the result in the temporary file
+    grep -v "$word" "$file" > "$temp_file"
+    
+    # Replace the original file with the temporary file
+    mv "$temp_file" "$file"
+    
+    echo "Deleted lines containing '$word' from $file."
 done
